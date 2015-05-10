c4c27e5ee421763d005b413384a96b0eb1859c56
joel <joel_madureira@hotmail.com>
2015-05-09 23:19:25 -0400

    ..
    ..

--- plugin.video.direto/default.py
+++ plugin.video.direto/default.py
@@ -1,80 +1,149 @@
-# coding: utf-8
+import urllib,urllib2,sys,re,xbmcplugin,xbmcgui,xbmcaddon,xbmc,os,json,base64
 
-import xbmcgui,xbmcplugin
+AddonID ='plugin.video.mksports'
+fanart = xbmc.translatePath(os.path.join('special://home/addons/' + AddonID , 'fanart.jpg'))
+icon = xbmc.translatePath(os.path.join('special://home/addons/' + AddonID, 'icon.png'))
+    
+def menu():
+    addDir('MK Sports Original Collection','url',1,icon,fanart)
+    addDir('BVLS 2013','url',2,icon,fanart)
+    
+def MKSports():
+    channelurl='http://www.softmagnate.com/CMS-Server-Pak-Hind-HD/getJson.php'
+    response=Get_url(channelurl)
+    link=json.loads(response)
+    for field in link:
+        categoryName= field['categoryName'].encode("utf-8")
+        channelLink= field['channelLink'].encode("utf-8")
+        channelName=field['channelName'].encode("utf-8")
+        if categoryName == 'Sports' or categoryName == 'Sports HD' or categoryName == 'Sports Time TV' or categoryName == 'Bein Sports' or categoryName == 'OTE Sports' or categoryName == 'Football' or categoryName == 'Cricket World Cup 2015' or categoryName == 'Sports Full HD':
 
-plugin_handle = int(sys.argv[1])
-_id = 'plugin.video.direto'
-_icondir = "special://home/addons/" + _id + "/icons/"
+            if not "3pm" in channelName:
+                addLink(channelName,channelLink,100,icon,fanart)
+    xbmcplugin.addSortMethod(int(sys.argv[1]), xbmcplugin.SORT_METHOD_VIDEO_TITLE)
 
-def add_video_item(url, infolabels, img=''):
-    listitem = xbmcgui.ListItem(infolabels['title'], iconImage=img, thumbnailImage=img)
-    listitem.setInfo('video', infolabels)
+def BVLS():
+    addDir('Daily Schedule','http://bvls2013.com/index.html',4,icon,fanart)
+    addLink('Stream 1','http://bvls2013.com/stream1.html',3,icon,fanart)
+    addLink('Stream 2','http://bvls2013.com/stream2.html',3,icon,fanart)
+    addLink('Stream 3','http://bvls2013.com/stream3.html',3,icon,fanart)
+    addLink('Stream 4','http://bvls2013.com/stream4.html',3,icon,fanart)
+    addLink('Stream 5','http://bvls2013.com/stream5.html',3,icon,fanart)
+    addLink('Stream 6','http://bvls2013.com/stream6.html',3,icon,fanart)
+    addLink('Stream 7','http://bvls2013.com/stream7.html',3,icon,fanart)
+    addLink('Stream 8','http://bvls2013.com/stream8.html',3,icon,fanart)
+    addLink('Stream 9','http://bvls2013.com/stream9.html',3,icon,fanart)
+    addLink('Stream 10','http://bvls2013.com/stream10.html',3,icon,fanart)
+
+def GetBVLSStream(name,url):
+    link = Get_url(url)
+    match=re.compile('src="(.+?)" id="myfr"').findall(link)[0]
+    link = Get_url(match)
+    match=re.compile("file\s*\:\s*window.atob\('(.+?)'\)").findall(link)[0]
+    temp_url = base64.b64decode(match)
+    veetleId = getVeetleIdByUrl(temp_url)
+    url = 'plugin://plugin.video.veetle/?channel='+veetleId
+    playlist = xbmc.PlayList(1)
+    playlist.clear()
+    listitem = xbmcgui.ListItem(name + " - " + url, iconImage=icon, thumbnailImage=icon)
+    listitem.setInfo("Video", {"Title":name})
+    listitem.setProperty('mimetype', 'video/x-msvideo')
     listitem.setProperty('IsPlayable', 'true')
-    xbmcplugin.addDirectoryItem(plugin_handle, url, listitem, isFolder=False)
+    playlist.add(url,listitem)
+    xbmcPlayer = xbmc.Player(xbmc.PLAYER_CORE_AUTO)
+    xbmcPlayer.play(playlist)
 
-# Entertainment
-add_video_item('http://link.theplatform.com/s/dtjsEC/77l1KA_G_5P1?feed=DEV%20Live%20Stream%20Feed',{ 'title': 'Global TV Halifax HD'}, '%s/global_tv_halifax.png'% _icondir)
+def getVeetleIdByUrl(url):
+    _regex_getM3u = re.compile("http://(.*?)/flv/(.*?)/1.flv", re.DOTALL)
+    streamId = _regex_getM3u.search(url).group(2)
+    return streamId
 
-add_video_item('http://link.theplatform.com/s/dtjsEC/EqBPGYLY6fEx?feed=DEV%20Live%20Stream%20Feed',{ 'title': 'Global TV Vancouver HD'}, '%s/global_tv_vancouver.png'% _icondir)
+def BVLSSched(url):
+    link = Get_url(url)
+    datestr=re.compile('<span style="text-decoration: underline;"><b>(.+?)</b></span></span></div>').findall(link)[0]
+    datestr = "[B][COLOR gold]"+datestr+"[/COLOR][/B]"
+    addLink(datestr,'url','mode',icon,fanart)
+    match=re.compile('12px;">(.+?)</span></p>.+?#000000">(.+?)</span></b></p>.+?<p><a href=".+?">(.+?)</a></p>',re.DOTALL).findall(link)
+    for timestr, name, stream in match:
+        name = timestr + " == " + name + " (" + stream + ")"
+        addLink(name,'url','mode',icon,fanart)
 
-add_video_item('http://link.theplatform.com/s/dtjsEC/hdSH3iztz_j_?feed=DEV%20Live%20Stream%20Feed',{ 'title': 'Global TV Toronto HD'}, '%s/global_tv_toronto.png'% _icondir)
+def Get_url(url):
+    req = urllib2.Request(url)
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+    response = urllib2.urlopen(req)
+    link=response.read()
+    response.close()
+    return link
+    
+def Play(name,url):  
+    ok=True
+    liz=xbmcgui.ListItem(name, iconImage=icon,thumbnailImage=icon); liz.setInfo( type="Video", infoLabels={ "Title": name } )
+    ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
+    xbmc.Player().play(url, liz, False)
+    return ok
 
-add_video_item('rtmp://54.85.197.21:1935/live/news live=1 timeout=15',{ 'title': 'CHCH Hamilton HD'}, '%s/CHCH.png'% _icondir)
+def addLink(name,url,mode,iconimage,fanart,description=''):
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&description="+str(description)
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name, 'plot': description } )
+        liz.setProperty('fanart_image', fanart)
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False)
+        return ok
 
-add_video_item('http://198.179.31.198/live/2059.high.stream/2059.high.stream/index.m3u8',{ 'title': 'CFCN CTV Lethbridge'}, '%s/CTV_Lethbridge.png'% _icondir)
+def addDir(name,url,mode,iconimage,fanart,description=''):
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&description="+str(description)
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name, 'plot': description } )
+        liz.setProperty('fanart_image', fanart)
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+        return ok
 
-add_video_item('http://198.179.31.198/live/2053.high.stream/2053.high.stream/index.m3u8',{ 'title': 'CFRN CTV Edmonton'}, '%s/CTV_Edmonton.png'% _icondir)
+def get_params():
+        param=[]
+        paramstring=sys.argv[2]
+        if len(paramstring)>=2:
+                params=sys.argv[2]
+                cleanedparams=params.replace('?','')
+                if (params[len(params)-1]=='/'):
+                        params=params[0:len(params)-2]
+                pairsofparams=cleanedparams.split('&')
+                param={}
+                for i in range(len(pairsofparams)):
+                        splitparams={}
+                        splitparams=pairsofparams[i].split('=')
+                        if (len(splitparams))==2:
+                                param[splitparams[0]]=splitparams[1]    
+        return param
+           
+params=get_params()
+url=None
+name=None
+mode=None
+iconimage=None
+description=None
 
-add_video_item('http://198.179.31.198/live/2062.high.stream/2062.high.stream/index.m3u8',{ 'title': 'CICC CTV Yorkton'}, '%s/CTV_Yorkton.png'% _icondir)
+try:url=urllib.unquote_plus(params["url"])
+except:pass
+try:name=urllib.unquote_plus(params["name"])
+except:pass
+try:mode=int(params["mode"])
+except:pass
+try:iconimage=urllib.unquote_plus(params["iconimage"])
+except:pass
+print "Mode: "+str(mode);print "URL: "+str(url);print "Name: "+str(name);print "IconImage: "+str(iconimage)
 
-add_video_item('http://198.179.31.198/live/2056.high.stream/2056.high.stream/index.m3u8',{ 'title': 'CKCK CTV Regina'}, '%s/CTV_Regina.png'% _icondir)
+if mode==None or url==None or len(url)<1:menu()
+elif mode==1:MKSports()
+elif mode==2:BVLS()
+elif mode==3:GetBVLSStream(name,url)
+elif mode==4:BVLSSched(url)
 
-add_video_item('http://tscstreaming-lh.akamaihd.net/i/TSCLiveStreaming_1@91031/index_3_av-p.m3u8',{ 'title': 'The Shopping Channel HD'}, '%s/tsc.png'% _icondir)
-
-add_video_item('http://link.theplatform.com/s/dtjsEC/chjdYJxKb44V?manifest=m3u&feed=History%20Live%20Stream%20Feed',{ 'title': 'History (Canada) HD'}, '%s/history.png'% _icondir)
 
 
 
-# News
-add_video_item('http://bcoveliveios-i.akamaihd.net/hls/live/207737/1942203455001/nat/master_Layer5.m3u8',{ 'title': 'Weather Network National HD'}, '%s/twn.png' % _icondir)
+elif mode==100:Play(name,url)
 
-#DOWN add_video_item('http://bcoveliveios-i.akamaihd.net/hls/live/207737/1942203455001/atl/master.m3u8',{ 'title': 'Weather Network Atlantic HD'}, '%s/twn.png' % _icondir)
-
-add_video_item('rtmp://a.cdn.newschat.tv/edge playpath=cbc_live swfUrl=http://msnbclive.eu/player.swf pageUrl=http://www.livenewschat.eu/canada/ live=1',{ 'title': 'CBC News'}, '%s/cbcnewsnetwork.png' % _icondir)
-
-add_video_item('http://ams-lp3.9c9media.com/hls-live/livepkgr/_definst_/liveeventNoDRM/cp24Open8.m3u8',{ 'title': 'CP24 News HD'}, '%s/CP24.png' % _icondir)
-
-add_video_item('http://ams-lp2.9c9media.com/hls-live/livepkgr/_definst_/liveeventNoDRM/ctvnewsOpen8.m3u8',{ 'title': 'CTV News HD'}, '%s/ctv_news.png' % _icondir)
-
-add_video_item('http://ams-lp10.9c9media.com/hls-live/livepkgr/_definst_/liveNews/News18.m3u8',{ 'title': 'CTV News Live Feed 1 HD'}, '%s/CTVNews.png' % _icondir)
-
-add_video_item('http://ams-lp7.9c9media.com/hls-live/livepkgr/_definst_/liveNews/News28.m3u8',{ 'title': 'CTV News Live Feed 2 HD'}, '%s/CTVNews.png' % _icondir)
-
-add_video_item('http://ams-lp6.9c9media.com/hls-live/livepkgr/_definst_/liveNews/News38.m3u8',{ 'title': 'CTV News Live Feed 3 HD'}, '%s/CTVNews.png' % _icondir)
-
-add_video_item('http://ams-lp5.9c9media.com/hls-live/livepkgr/_definst_/liveNews/News48.m3u8',{ 'title': 'CTV News Live Feed 4 HD'}, '%s/CTVNews.png' % _icondir)
-
-# Sports
-add_video_item('http://nlds187.cdnak.neulion.com/nlds/sportsnetnow/sn_360/as/live/sn_360_hd_ipad.m3u8',{ 'title': 'Sportsnet 360 HD'}, '%s/sportsnet_360.png'% _icondir)
-
-#DOWN add_video_item('http://ams-lp5.9c9media.com/hls-live/livepkgr/_definst_/liveeventNoDRM/tsnOpen8.m3u8',{ 'title': 'TSN1 HD'}, '%s/tsn_1.png'% _icondir)
-
-#DOWN add_video_item('http://ams-lp7.9c9media.com/hls-live/livepkgr/_definst_/liveeventNoDRM/TSN2Open8.m3u8',{ 'title': 'TSN2 HD'}, '%s/tsn_2.png'% _icondir)
-
-#DOWN add_video_item('http://ams-lp1.9c9media.com/hls-live/livepkgr/_definst_/liveeventNoDRM/TSN3Open8.m3u8',{ 'title': 'TSN3 HD)'}, '%s/tsn_3.png'% _icondir)
-
-#DOWN add_video_item('http://ams-lp2.9c9media.com/hls-live/livepkgr/_definst_/liveeventNoDRM/TSN4Open8.m3u8',{ 'title': 'TSN4 HD'}, '%s/tsn_4.png'% _icondir)
-
-#DOWN add_video_item('http://ams-lp3.9c9media.com/hls-live/livepkgr/_definst_/liveeventNoDRM/TSN5Open8.m3u8',{ 'title': 'TSN5 HD'}, '%s/tsn_5.png'% _icondir)
-
-add_video_item('http://ams-lp9.9c9media.com/hls-live/livepkgr/_definst_/liveeventNoDRM/RDSOpen8.m3u8',{ 'title': 'RDS (French) HD'}, '%s/rds.png'% _icondir)
-
-add_video_item('http://ams-lp10.9c9media.com/hls-live/livepkgr/_definst_/liveeventNoDRM/RDS2Open8.m3u8',{ 'title': 'RDS2 (French) HD'}, '%s/rds2.png'% _icondir)
-
-add_video_item('http://ams-lp11.9c9media.com/hls-live/livepkgr/_definst_/liveeventNoDRM/RDSiOpen8.m3u8',{ 'title': 'RDS Info(French) HD'}, '%s/rds_info.png'% _icondir)
-
-add_video_item('rtmp://cp39414.live.edgefcs.net/live/event1@9756',{ 'title': 'TVA Sports (French)'}, '%s/TVA_Sports.png'% _icondir)
-
-xbmcplugin.endOfDirectory(plugin_handle)
-xbmc.executebuiltin("Container.SetViewMode(500)")
-
+xbmcplugin.endOfDirectory(int(sys.argv[1]))
