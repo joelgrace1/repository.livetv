0e6fd71d70c1fee5cc47ef28b1909f717affc8b6
joel <joel_madureira@hotmail.com>
2015-05-09 23:54:44 -0400

    ..
    ..

--- plugin.video.direto/default.py
+++ plugin.video.direto/default.py
@@ -5,10 +5,10 @@ fanart = xbmc.translatePath(os.path.join('special://home/addons/' + AddonID , 'f
 icon = xbmc.translatePath(os.path.join('special://home/addons/' + AddonID, 'icon.png'))
     
 def menu():
-    addDir('MK Sports Original Collection','url',1,icon,fanart)
-    addDir('BVLS 2013','url',2,icon,fanart)
+    addDir('PORTUGAL','url',1,icon,fanart)
     
-def MKSports():
+    
+def PORTUGAL():
     channelurl='http://www.softmagnate.com/CMS-Server-Pak-Hind-HD/getJson.php'
     response=Get_url(channelurl)
     link=json.loads(response)
@@ -22,51 +22,6 @@ def MKSports():
                 addLink(channelName,channelLink,100,icon,fanart)
     xbmcplugin.addSortMethod(int(sys.argv[1]), xbmcplugin.SORT_METHOD_VIDEO_TITLE)
 
-def BVLS():
-    addDir('Daily Schedule','http://bvls2013.com/index.html',4,icon,fanart)
-    addLink('Stream 1','http://bvls2013.com/stream1.html',3,icon,fanart)
-    addLink('Stream 2','http://bvls2013.com/stream2.html',3,icon,fanart)
-    addLink('Stream 3','http://bvls2013.com/stream3.html',3,icon,fanart)
-    addLink('Stream 4','http://bvls2013.com/stream4.html',3,icon,fanart)
-    addLink('Stream 5','http://bvls2013.com/stream5.html',3,icon,fanart)
-    addLink('Stream 6','http://bvls2013.com/stream6.html',3,icon,fanart)
-    addLink('Stream 7','http://bvls2013.com/stream7.html',3,icon,fanart)
-    addLink('Stream 8','http://bvls2013.com/stream8.html',3,icon,fanart)
-    addLink('Stream 9','http://bvls2013.com/stream9.html',3,icon,fanart)
-    addLink('Stream 10','http://bvls2013.com/stream10.html',3,icon,fanart)
-
-def GetBVLSStream(name,url):
-    link = Get_url(url)
-    match=re.compile('src="(.+?)" id="myfr"').findall(link)[0]
-    link = Get_url(match)
-    match=re.compile("file\s*\:\s*window.atob\('(.+?)'\)").findall(link)[0]
-    temp_url = base64.b64decode(match)
-    veetleId = getVeetleIdByUrl(temp_url)
-    url = 'plugin://plugin.video.veetle/?channel='+veetleId
-    playlist = xbmc.PlayList(1)
-    playlist.clear()
-    listitem = xbmcgui.ListItem(name + " - " + url, iconImage=icon, thumbnailImage=icon)
-    listitem.setInfo("Video", {"Title":name})
-    listitem.setProperty('mimetype', 'video/x-msvideo')
-    listitem.setProperty('IsPlayable', 'true')
-    playlist.add(url,listitem)
-    xbmcPlayer = xbmc.Player(xbmc.PLAYER_CORE_AUTO)
-    xbmcPlayer.play(playlist)
-
-def getVeetleIdByUrl(url):
-    _regex_getM3u = re.compile("http://(.*?)/flv/(.*?)/1.flv", re.DOTALL)
-    streamId = _regex_getM3u.search(url).group(2)
-    return streamId
-
-def BVLSSched(url):
-    link = Get_url(url)
-    datestr=re.compile('<span style="text-decoration: underline;"><b>(.+?)</b></span></span></div>').findall(link)[0]
-    datestr = "[B][COLOR gold]"+datestr+"[/COLOR][/B]"
-    addLink(datestr,'url','mode',icon,fanart)
-    match=re.compile('12px;">(.+?)</span></p>.+?#000000">(.+?)</span></b></p>.+?<p><a href=".+?">(.+?)</a></p>',re.DOTALL).findall(link)
-    for timestr, name, stream in match:
-        name = timestr + " == " + name + " (" + stream + ")"
-        addLink(name,'url','mode',icon,fanart)
 
 def Get_url(url):
     req = urllib2.Request(url)
@@ -136,10 +91,9 @@ except:pass
 print "Mode: "+str(mode);print "URL: "+str(url);print "Name: "+str(name);print "IconImage: "+str(iconimage)
 
 if mode==None or url==None or len(url)<1:menu()
-elif mode==1:MKSports()
+elif mode==1:PORTUGAL()
 elif mode==2:BVLS()
-elif mode==3:GetBVLSStream(name,url)
-elif mode==4:BVLSSched(url)
+
 
 
 
