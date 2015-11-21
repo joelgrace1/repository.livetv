839ded67ad83d2b287ff2c02ecaea60c8afbba07
joel <joel_madureira@hotmail.com>
2015-11-21 15:56:33 -0500

    maniac v5 final update

--- plugin.video.ManiaC/default.py
+++ plugin.video.ManiaC/default.py
@@ -12,14 +12,14 @@ import ntpath
 
 USER_AGENT = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3'
 base='www.ManiaC.com'
-ADDON=xbmcaddon.Addon(id='plugin.video.ManiaC V5')
+ADDON=xbmcaddon.Addon(id='plugin.video.ManiaC V5 Final')
 dialog = xbmcgui.Dialog()    
 VERSION = "1.0.2"
-PATH = "ManiaC"            
+PATH = "ManiaC V5"            
 
     
 def CATEGORIES():
-    link = OPEN_URL('https://copy.com/oYHQBObUWcmEWO9k').replace('\n','').replace('\r','')
+    link = OPEN_URL('https://copy.com/w4LPlZc04pVVuab1').replace('\n','').replace('\r','')
     match = re.compile('name="(.+?)".+?rl="(.+?)".+?mg="(.+?)".+?anart="(.+?)".+?escription="(.+?)"').findall(link)
     for name,url,iconimage,fanart,description in match:
         addDir(name,url,1,iconimage,fanart,description)
